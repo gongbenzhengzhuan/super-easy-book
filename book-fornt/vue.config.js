8283b78734eda14d4d6a868d897da733c8e55963
gongbenzheng <1459490255@qq.com>
2023-05-26 21:27:30 +0800

    解决304

--- book-fornt/vue.config.js
+++ book-fornt/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? './' : './',
+  outputDir: 'dist',
+  indexPath: 'index.html',
   lintOnSave: false,
   devServer:{
     port:8020, // 启动端口号
